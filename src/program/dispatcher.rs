6819ac678315da6724aa3dff15183b488871e104
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-09-05 12:43:22 -0400

    implemented required method addBus at an index.

--- src/program/dispatcher.rs
+++ src/program/dispatcher.rs
@@ -7,4 +7,15 @@ impl Dispatcher {
     pub fn add_bus(&mut self, bus: Bus) {
         self.bus_queue.push(bus);
     }
+
+    // add_bus() "overload"
+    pub fn insert_bus(&mut self, bus: Bus, queue_position: usize) -> usize {
+        if queue_position > self.bus_queue.len() {
+            self.bus_queue.push(bus);
+            return self.bus_queue.len();
+        } else {            
+            self.bus_queue.insert(queue_position, bus);
+            return queue_position;
+        }
+    }
 }

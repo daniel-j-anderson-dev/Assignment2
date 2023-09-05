1a56ad2d49bd9dd6bc85d4e012919c75187b32a0
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-09-05 13:25:08 -0400

    Implemented menu option 1

--- src/program.rs
+++ src/program.rs
@@ -1,14 +1,23 @@
 mod bus;
 mod person;
 mod dispatcher;
+
+use crate::program::{person::Person, bus::Bus, dispatcher::Dispatcher};
+
 pub struct Driver {}
 impl Driver {
     pub fn main() {
+        let mut dispatcher: Dispatcher = Dispatcher::new();
         println!("Bus Dispatching System\n");
         let menu: &str ="1. Add bus\n2. Add Person to bus\n3. Remove bus\n4. Remove person\n5. List passengers\n6. List busses\n7. Requeue bus\n8. Transfer person\n9. Dispatch bus\n0. Exit\n";
         loop {
-            match Driver::get_input(menu) {
-                1 => {}
+            match Driver::get_parsed_input(menu) {
+                1 => {
+                    let bus: Bus = Bus::default();
+                    let id: usize = bus.get_id();
+                    let queue_position: usize = dispatcher.add_bus(bus);
+                    println!("Bus {id} added to position {queue_position}");
+                }
                 2 => {}
                 3 => {}
                 4 => {}
@@ -23,13 +32,13 @@ impl Driver {
         }
     }
 
-    fn get_input<T>(prompt: &str) -> T 
+    fn get_parsed_input<T>(prompt: &str) -> T 
     where
         T: std::str::FromStr,
         <T as std::str::FromStr>::Err: std::error::Error,
     {
         loop {
-            match Driver::read_line(prompt).parse() {
+            match Driver::get_raw_input(prompt).parse() {
                 Ok(input) => return input,
                 Err(parse_error) => {
                     eprintln!("\nInvalid input: {parse_error}\n");
@@ -39,7 +48,7 @@ impl Driver {
         }
     }
 
-    fn read_line(prompt: &str) -> String {
+    fn get_raw_input(prompt: &str) -> String {
         use std::io::{stdin, stdout, Write};
         
         loop {

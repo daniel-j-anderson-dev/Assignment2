c30fa6742d9c5d2f7c28f793dc731594ca27cb52
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-09-05 12:42:40 -0400

    implemented required method addBus.

--- src/program/dispatcher.rs
+++ src/program/dispatcher.rs
@@ -3,4 +3,8 @@ use crate::program::bus::Bus;
 pub struct Dispatcher {
     bus_queue: Vec<Bus>
 }
-impl Dispatcher {}
+impl Dispatcher {
+    pub fn add_bus(&mut self, bus: Bus) {
+        self.bus_queue.push(bus);
+    }
+}

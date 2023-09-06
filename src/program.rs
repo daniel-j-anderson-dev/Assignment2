7273e001910b9330274a5bda685b29717eb7224f
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-09-06 13:05:38 -0400

    Fixed 4, and added 5-9

--- src/program.rs
+++ src/program.rs
@@ -24,7 +24,7 @@ impl Driver {
                             let name: String = Driver::get_raw_input("Enter person's name: ");
                             let person: Person = Person::new(name.as_str());
                             bus.add_person(person);
-                            println!("\n{name} has been added to bus {id}")
+                            println!("{name} has been added to bus {id}")
                         }
                         None => println!("No bus with id {id}"),
                     }
@@ -32,43 +32,98 @@ impl Driver {
                 3 => {
                     let id: usize = Driver::get_parsed_input("Enter bus id: ");
                     match dispatcher.remove_bus(id) {
-                        Some((removed_bus, _queue_position)) => println!("bus {removed_bus} removed"),
+                        Some(removed_bus) => println!("bus {removed_bus} removed"),
                         None => println!("No bus with id {id}"),
                     }
                 }
                 4 => {
                     let id: usize = Driver::get_parsed_input("Enter bus id: ");
-                    let name: String;
-                    let bus: &mut Bus;
-                    let bus_clone: Bus;
-                    let person: Person;
-
                     match dispatcher.find_bus_mut(id) {
                         Some(found_bus) => {
-                            name = Driver::get_raw_input("Enter person's name: ");
-                            bus_clone = found_bus.clone();    
-                            match bus_clone.find_person(&name) {
-                                Some(found_person) => person = found_person,
+                            let name: String = Driver::get_raw_input("Enter person's name: ");
+                            let person: Person = match found_bus.clone().find_person(&name) {
+                                Some(found_person) => found_person.clone(),
+                                None => {
+                                    println!("No such person found in bus {id}");
+                                    continue;
+                                }
+                            };
+                            match found_bus.remove_person(&person) {
+                                Some(removed_person) => println!("{} has been removed from bus {id}", removed_person.get_name()),
                                 None => {
                                     println!("No such person found in bus {id}");
                                     continue;
                                 }
                             }
-                            bus = found_bus
                         },
                         None => {
                             println!("No bus with id {id}");
                             continue;
                         }
                     };
-
-                    bus.remove_person(&person);
                 }
-                5 => {}
-                6 => {}
-                7 => {}
-                8 => {}
-                9 => {}
+                5 => {
+                    let id: usize = Driver::get_parsed_input("Enter bus id: ");
+                    match dispatcher.find_bus(id) {
+                        Some(found_bus) => {
+                            print!("Bus id {}\n{}", found_bus.get_id(), found_bus.get_passengers());
+                        }
+                        None => {
+                            println!("No bus with id {id}");
+                            continue;
+                        }
+                    }
+                }
+                6 => {
+                    print!("BUS QUEUE\n{dispatcher}");
+                }
+                7 => {
+                    let id: usize = Driver::get_parsed_input("Enter bus id: ");
+                    match dispatcher.remove_bus(id) {
+                        Some(found_bus) => {
+                            let new_queue_position: usize = Driver::get_parsed_input("Enter new bus position: ");
+                            dispatcher.insert_bus(found_bus, new_queue_position);
+                        }
+                        None => {
+                            println!("No bus with id {id}");
+                            continue;
+                        }
+                    }
+                }
+                8 => {
+                    // didnt use transfer_person but it still works because remove person returns an Option<person> instead of bool
+                    let name: String = Driver::get_raw_input("Enter person's name: ");
+                    let source_bus_id: usize = Driver::get_parsed_input("Enter id of bus 1: ");
+                    let destination_bus_id: usize = Driver::get_parsed_input("Enter id of bus 2: ");
+                    let person: Person = match dispatcher.find_bus_mut(source_bus_id) {
+                        Some(source_bus) => {
+                            match source_bus.clone().find_person(&name) {
+                                Some(person) => source_bus.remove_person(person).unwrap(),
+                                None => {
+                                    println!("No person named {name} in bus {source_bus_id}");
+                                    continue;
+                                }
+                            }
+                        }
+                        None => {
+                            println!("No bus with id {source_bus_id}");
+                            continue;
+                        }
+                    };
+                    match dispatcher.find_bus_mut(destination_bus_id) {
+                        Some(destination_bus) => destination_bus.add_person(person),
+                        None => {
+                            println!("No bus with id {destination_bus_id}");
+                            continue;
+                        }
+                    }
+                }
+                9 => {
+                    match dispatcher.dispatch_bus() {
+                        Some(bus) => println!("Bus {} has been dispatched", bus.get_id()),
+                        None => println!("Bus queue is empty"),
+                    }
+                }
                 0 => {
                     println!("Shutting down");
                     break;
